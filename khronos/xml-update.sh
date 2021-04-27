44370d97cb8cdce9df55943698b075dae4e02395
viri <hi@viri.moe>
2021-04-27 03:09:28 -0600

    khronos: script stores `HEAD` SHA

--- khronos/xml-update.sh
+++ khronos/xml-update.sh
@@ -3,8 +3,16 @@
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-GL_XML='https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml'
+REPO="KhronosGroup/OpenGL-Registry"
+GL_XML="https://raw.githubusercontent.com/$REPO/master/xml/gl.xml"
 
-echo 'Updating XML registry...' && \
-    echo '> Re-downloading `gl.xml`' && curl -sSf -o "$SCRIPT_DIR/gl.xml" "$GL_XML" && \
-    echo 'Done!'
+true && \
+    printf "> Re-downloading \`gl.xml\`\n" && \
+        curl -sSf -o "$SCRIPT_DIR/gl.xml" "$GL_XML" && \
+    printf "> Logging SHA (GitHub API)\n" && \
+        curl -sSf -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/commits/HEAD" | \
+        perl -e 'use JSON; my $resp = decode_json(do { local $/; <STDIN> }); print $resp->{"sha"};' | \
+        printf "repo $REPO\nsha $(cat -)\n" \
+            > "$SCRIPT_DIR/commit"
+
+printf "Done!\n"
