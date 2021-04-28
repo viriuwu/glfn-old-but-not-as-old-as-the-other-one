14de4f8d5a9aa7220eef043efd9236f244093aaa
viri <hi@viri.moe>
2021-04-28 00:21:43 -0600

    khronos: commit `wgl.xml`

--- khronos/xml-update.sh
+++ khronos/xml-update.sh
@@ -11,6 +11,8 @@ true && \
         curl -sSf -o "$SCRIPT_DIR/gl.xml" "$XML_DL/gl.xml" && \
     printf "> Re-downloading \`glx.xml\`\n" && \
         curl -sSf -o "$SCRIPT_DIR/glx.xml" "$XML_DL/glx.xml" && \
+    printf "> Re-downloading \`wgl.xml\`\n" && \
+        curl -sSf -o "$SCRIPT_DIR/wgl.xml" "$XML_DL/wgl.xml" && \
     printf "> Logging SHA (GitHub API)\n" && \
         curl -sSf -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/commits/HEAD" | \
         perl -e 'use JSON; my $resp = decode_json(do { local $/; <STDIN> }); print $resp->{"sha"};' | \
