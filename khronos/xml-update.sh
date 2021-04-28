33eb4d6745e50f559a7c133830468643990cc4d6
viri <hi@viri.moe>
2021-04-28 00:21:31 -0600

    khronos: commit `glx.xml`

--- khronos/xml-update.sh
+++ khronos/xml-update.sh
@@ -4,11 +4,13 @@ SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
 REPO="KhronosGroup/OpenGL-Registry"
-GL_XML="https://raw.githubusercontent.com/$REPO/master/xml/gl.xml"
+XML_DL="https://raw.githubusercontent.com/$REPO/master/xml"
 
 true && \
     printf "> Re-downloading \`gl.xml\`\n" && \
-        curl -sSf -o "$SCRIPT_DIR/gl.xml" "$GL_XML" && \
+        curl -sSf -o "$SCRIPT_DIR/gl.xml" "$XML_DL/gl.xml" && \
+    printf "> Re-downloading \`glx.xml\`\n" && \
+        curl -sSf -o "$SCRIPT_DIR/glx.xml" "$XML_DL/glx.xml" && \
     printf "> Logging SHA (GitHub API)\n" && \
         curl -sSf -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPO/commits/HEAD" | \
         perl -e 'use JSON; my $resp = decode_json(do { local $/; <STDIN> }); print $resp->{"sha"};' | \
