b340bf6289029b44753ec2ee5da8586da80c201a
viri <hi@viri.moe>
2021-05-01 03:31:04 -0600

    root: impl `TryFrom<&str>` for `Api`

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@ pub mod reg;
 pub mod types;
 pub mod xml;
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+use std::convert::TryFrom;
+
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum Api {
     Gl,
-    NotGlPleaseDontGiveMeAnIrrefutableLint,
-    ApiNumberThree,
+    Gles2,
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -14,6 +15,17 @@ pub enum Platform {
     Windows,
     Linux,
     Mac,
+}
+
+impl TryFrom<&str> for Api {
+    type Error = ();
+    fn try_from(x: &str) -> Result<Self, Self::Error> {
+        match x {
+            "gl" => Ok(Api::Gl),
+            "gles2" => Ok(Api::Gles2),
+            _ => Err(()),
+        }
+    }
 }
 
 #[cfg(test)]
